a1167c4480dcf7419b117db77dcced5e297ef45d
BobHunt <bomazani@icloud.com>
2021-07-11 22:00:15 -0400

    Resolved issue with API fetch.

--- script.js
+++ script.js
@@ -8,49 +8,22 @@ const vehicles = `${baseUrl}vehicles`;
 let listDisplay = document.getElementById('ul-display');
 
 async function getGibli() {
-    try {
-        await fetch(vehicles)
-            .then(function(response){
-                console.log(`response type is: ${typeof response}`);
-                response.text();
-                console.log(`now response is: ${typeof response}`);
-                console.log(response.length);
-
-            .then(function(res){
-                let myList = res;
-                console.log(myList.length);
-                // console.log(res);
-                // console.log(typeof myList);
-                // for (item of myList){
-                //     console.log(`item: ${item}`);
-                    // let listItem = document.createElement('li');
-                    // listItem.innerHTML = '<p>' + item.name + '</p>';
-                    // listDisplay.appendChild(listItem);
-                }
-            );
+    await fetch(films)
+        .then(function(response){
+            // console.log(`response type is: ${typeof response}`);
+            // console.log(`now response is: ${typeof response}`);
+            // console.log(response.length);
+            return response.json()
         })
 
-    } catch (error) {
-        console.log(error);
-    }
-
+        .then((data) => {
+            //work with json data here
+            console.log(data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
 
 getGibli();
 
-// let GhibliList = document.querySelector('ul');
-
-// fetch()
-//     .then(function(response){
-//         return response.json();
-//         // console.log(response)
-//     })
-//     .then(function(json){
-//         // console.log(json.results);
-//         let listResults = json.results
-//         for (item of list){
-//             let listItem = document.createElement('li');
-//             listItem.innerHTML = '<p>' + person.name + '</p>';
-//             starWarsPeopleList.appendChild(listItem);
-//         }
-//     });
