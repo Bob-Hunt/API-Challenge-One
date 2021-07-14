b5e39184ecfbd6277b51dcff1a5532545f2373bb
BobHunt <bomazani@icloud.com>
2021-07-13 21:57:55 -0400

    All 5 HTML buttons are working with the chooseInput() function. Proper fetches are being consoled.

--- script.js
+++ script.js
@@ -1,29 +1,59 @@
+// URL variables
 const baseUrl = 'https://ghibliapi.herokuapp.com/';
-const films = `${baseUrl}films`;
-const people = `${baseUrl}people`;
-const species = `${baseUrl}species`;
-const locations = `${baseUrl}locations`;
-const vehicles = `${baseUrl}vehicles`;
+const filmsUrl = `${baseUrl}films`;
+const peopleUrl = `${baseUrl}people`;
+const speciesUrl = `${baseUrl}species`;
+const locationsUrl = `${baseUrl}locations`;
+const vehiclesUrl = `${baseUrl}vehicles`;
 
-let listDisplay = document.getElementById('ul-display');
+let myUrl;
 
-async function getGibli() {
-    await fetch(films)
+function chooseInput(mybutton){
+    console.log(mybutton.id);
+
+    switch (mybutton.id) {
+        case 'films':
+            console.log(1);
+            getFetch(filmsUrl);
+            break;
+        case 'people':
+            console.log(2);
+            getFetch(peopleUrl);
+            break;
+        case 'species':
+            console.log(3);
+            getFetch(speciesUrl);
+            break;
+        case 'locations':
+            console.log(4);
+            getFetch(locationsUrl);
+            break;
+        case 'vehicles':
+            console.log(5);
+            getFetch(vehiclesUrl);
+            break;
+        default:
+            break;
+    }
+}
+
+
+async function getFetch(myParameter){
+    console.log(`myParameter: ${myParameter}`);
+   
+    await fetch(myParameter)
         .then(function(response){
-            // console.log(`response type is: ${typeof response}`);
-            // console.log(`now response is: ${typeof response}`);
-            // console.log(response.length);
-            return response.json()
+            return response.json();
         })
 
         .then((data) => {
             //work with json data here
-            console.log(data)
+            console.log(`type: `,typeof data);
+            console.log(data.length);
+            console.log(data);
         })
         .catch((err) => {
-            console.log(err)
+            console.log(err);
         })
 }
-
-getGibli();
 
