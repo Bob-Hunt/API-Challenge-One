c6d9e7fe3a9d6740dba6faaeb2540322650ba901
BobHunt <bomazani@icloud.com>
2021-07-15 19:01:21 -0400

    Cleaned up code. Deleted unused code & whitespace. Added comments.

--- script.js
+++ script.js
@@ -11,33 +11,16 @@ let english = true;
 let showHideDiv = document.querySelector('.translate-container');
 showHideDiv.setAttribute('id', 'hide-translate-container');
 
-
 let displayItems;
 let displayOne = document.getElementById('ul-one');
 let displayTwo = document.getElementById('ul-two');
 let displayThree = document.getElementById('ul-three');
 
-// Multiple Background Images: Change On Button Click        //
-// From:  http://css3.bradshawenterprises.com/cfimg/#cfimg7  //
-$(document).ready(function() {
-    $("#cf7_controls").on('click', 'span', function() {
-      $("#cf7 img").removeClass("opaque");
-  
-      var newImage = $(this).index();
-  
-      $("#cf7 img").eq(newImage).addClass("opaque");
-  
-      $("#cf7_controls span").removeClass("selected");
-      $(this).addClass("selected");
-    });
-  });
-  
 
-// accepts event from 5 different HTML buttons and calls getFetch()
+// accepts an event from 1 of 5 different HTML buttons and calls getFetch()
 // with different parameters based on which button is clicked.
 function chooseInput(mybutton, language){
     console.log(mybutton.id);
-
     switch (mybutton.id) {
         case 'films':
             getFetch(filmsUrl, 'english');
@@ -59,9 +42,10 @@ function chooseInput(mybutton, language){
     }
 }
 
-function titleTranslate(transBtn){
-    console.log(transBtn.id);
 
+// Determines the second parameter to pass into getFetch(),
+// which will determine which key:value will be pulled from the films API.
+function titleTranslate(transBtn){
     switch (transBtn.id) {
         case 'english':
             getFetch(filmsUrl, 'english');
@@ -75,16 +59,13 @@ function titleTranslate(transBtn){
 }
 
 
-// getFetch function //
-async function getFetch(myParameter, language){
-    console.log(`myParameter: ${myParameter}`);
-    console.log(`language: ${language}`);
-   
+// getFetch function **Does way too much!
+// Should be refactored into multiple functions.
+async function getFetch(myParameter, language){   
     await fetch(myParameter)
         .then(function(response){
             return response.json();
         })
-
         .then((data) => {
             console.log(data.length);
             console.log(data);
@@ -92,24 +73,20 @@ async function getFetch(myParameter, language){
             // Removes results from previous button before adding results from current button.
             while (displayOne.firstChild) {
                 displayOne.removeChild(displayOne.firstChild);
-              };
-
+            };
             while (displayTwo.firstChild) {
                 displayTwo.removeChild(displayTwo.firstChild);
             };
-
             while (displayThree.firstChild) {
                 displayThree.removeChild(displayThree.firstChild);
             };
 
-             let count = 0
+            let count = 0
             for (item of data){
                 count += 1;
                 let listItem = document.createElement('li');
                 // Films has a key of 'title' instead of 'name', 
-                // checks for key of 'title', if exists, returns 'title' instead of 'name'.
-                console.log(count);
-
+                // checks for key of 'title', if exists, uses 'title' key instead of 'name' key.
                 if (item.hasOwnProperty('title')) {
                     if (language === 'english'){
                         listItem.innerHTML = '<p>' + item.title + '</p>';
@@ -118,11 +95,11 @@ async function getFetch(myParameter, language){
                     }
                     } else if (language === 'romanji'){
                         listItem.innerHTML = '<p>' + item.original_title_romanised +  '</p>';
-                
                 } else {
                     listItem.innerHTML = '<p>' + item.name + '</p>';
                 }
-            
+   
+                //  Based on each item's 'count' it assigns it to one of 3 columns
                 if (count %3 === 0){
                     displayItems = document.querySelector('#ul-three');
                 } else if (count %3 === 2){
@@ -130,13 +107,9 @@ async function getFetch(myParameter, language){
                 } else if (count %3 === 1){
                     displayItems = document.querySelector('#ul-one');
                 }
-
                 displayItems.appendChild(listItem);  
+            };
 
-            }
-
-            // showHideDiv = document.querySelector('.translate-container');
-            // console.log(showHideDiv.id);
             switch(language){
                 case 'english':
                     showHideDiv.setAttribute('id', 'show-translate-container');
@@ -151,13 +124,8 @@ async function getFetch(myParameter, language){
                     showHideDiv.setAttribute('id', 'hide-translate-container');
                     break;
                 }
-                // console.log(showHideDiv.id);
-        
         })
         .catch((err) => {
             console.log(err);
         })
 }
-
-// Possibly, dynamically create a Translate button that translates all film
-// titles when pressed (clears element and reprints english AND Japanese)
